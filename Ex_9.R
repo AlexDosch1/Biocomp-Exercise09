dc0976c8ff9a4a532439c383e7d0f5a888a62b53
AlexDosch1 <91433155+AlexDosch1@users.noreply.github.com>
2021-11-17 11:28:24 -0500

    Add files via upload

---
+++ Ex_9.R
@@ -0,0 +1,7 @@
+## write function that takes directory name as argument called dir 
+    #and any other necessary arguments
+
+# read data from each file in the directory and calculate coefficient of 
+  # variation (=stdv/mean) ofr a user specified column; return as vector
+
+# default to report error for <50 observations, with override power to user
