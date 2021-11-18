d2cf867aa7b9a456cd8602fe480e513937ec41a8
AlexDosch1 <91433155+AlexDosch1@users.noreply.github.com>
2021-11-18 13:17:33 -0500

    Add files via upload

--- Ex_9.R
+++ Ex_9.R
@@ -5,3 +5,16 @@
   # variation (=stdv/mean) ofr a user specified column; return as vector
 
 # default to report error for <50 observations, with override power to user
+
+#set up empty list for output 
+output <- NULL
+
+#for loop to iterate calcs for each file
+function_name <- function(dir)
+for (file in dir) {
+  data <- read.table("*.txt", header=TRUE,sep=" ",stringsAsFactors=FALSE)
+ if ()
+   sd(file[i],)
+}
+#another loop to put data into a single vector (name of list.append (or something like that))
+#use if-else boi to handle the 50 observations problem (put in the first loop)
