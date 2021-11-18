24717a3930f3cfb1db4a1775e9a94ebae297ee20
AlexDosch1 <91433155+AlexDosch1@users.noreply.github.com>
2021-11-18 14:57:43 -0500

    Add files via upload

--- Ex_9.R
+++ Ex_9.R
@@ -1,20 +1,35 @@
 ## write function that takes directory name as argument called dir 
     #and any other necessary arguments
-
 # read data from each file in the directory and calculate coefficient of 
-  # variation (=stdv/mean) ofr a user specified column; return as vector
-
+  # variation (=stdv/mean) of a user specified column; return as vector
 # default to report error for <50 observations, with override power to user
 
 #set up empty list for output 
-output <- NULL
-
-#for loop to iterate calcs for each file
-function_name <- function(dir)
+output <- vector(mode="numeric")
+answer <- vector( mode="logical", length=1L)
+  # write function name with inputs dir= directory and col = user-specified
+  #column for the calculations
+cvCalc <- function(dir, col)
+  #for loop to iterate calculations in each file
+    #read file
+    #calculate the coefficient of variation
 for (file in dir) {
-  data <- read.table("*.txt", header=TRUE,sep=" ",stringsAsFactors=FALSE)
- if ()
-   sd(file[i],)
+  data <- read.table("file", header=TRUE,sep=",",stringsAsFactors=FALSE)
+  #if-else statement: for files with at least 50 observations, run the code 
+  if (length(data[,col]) >= 50) {
+    cvVal <- sd((data[, col]))/mean(data[,col])
+    output<- append(output, cvCalc)
+    return(output)
+    # for files with fewer than 50 observations, print warning, if they want to 
+      #continue they can, if not, loop breaks
+  } else if (length(data[,col]) < 50 ) {
+  print("Warning: less than 50 observations")
+  answer <-readline("Would you like to continue anyway? Type TRUE to continue or FALSE to stop")
+  if (answer=="TRUE") {
+    cvVal <- sd((data[, col]))/mean(data[,col])
+    output<- append(output, cvCalc)
+  } else {
+    break
+  }
 }
-#another loop to put data into a single vector (name of list.append (or something like that))
-#use if-else boi to handle the 50 observations problem (put in the first loop)
+}
